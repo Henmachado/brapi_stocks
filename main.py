219fe6b4ae0c3a8cfe7ecc49405deee993f2df60
Henrique Machado <henriquem@Henriques-Mac-mini.local>
2026-06-07 12:08:30 -0300

    add free and full-api modes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 import utils
 from api_ingestion import brapi_api_consumer
 
@@ -7,6 +8,13 @@ logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Brapi Stock Data Ingestion")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-free", action="store_true", help="Ingest only free stock tickers")
+    group.add_argument("-full-api", action="store_true", help="Ingest all active stock tickers")
+    
+    args = parser.parse_args()
+
     # Fetch all active stock tickers (~2,280 items on jun/2026)
     active_tickers_response = brapi_api_consumer.get_active_stock_tickers()
 
@@ -15,16 +23,18 @@ if __name__ == "__main__":
     active_tickers_list: list[str] = [stock['stock'] for stock in active_tickers_response]
     logger.info(f"Total Active Stocks Found: {len(active_tickers_list)} \n")
 
-    # To ingest all available modules, this behaviour is set as default.
-    # You will need to have a pro api plan to ingest all modules for all
-    # public available stocks.
-
-    stock_list = brapi_api_consumer.FREE_STOCKS_TICKERS  # replace for active_tickers_list in pro api plan
+    # Set stock_list based on CLI arguments
+    if args.full_api:
+        logger.info("Running in FULL mode: Ingesting all active tickers.\n")
+        stock_list = active_tickers_list
+    else:
+        logger.info("Running in FREE mode: Ingesting default free tickers.\n")
+        stock_list = brapi_api_consumer.FREE_STOCKS_TICKERS
 
     # Fetch data for all available modules
-    for _module in brapi_api_consumer.API_MODULES:
-        api_response = brapi_api_consumer.fetch_api_data_per_ticker_batch(stock_list=stock_list, module=_module)
-        utils.save_json_data(data=api_response, file_name=_module.lower())
+    # for _module in brapi_api_consumer.API_MODULES:
+    #     api_response = brapi_api_consumer.fetch_api_data_per_ticker_batch(stock_list=stock_list, module=_module)
+    #     utils.save_json_data(data=api_response, file_name=_module.lower())
 
     # Fetch data for default api response (no module passed on request)
     api_response = brapi_api_consumer.fetch_api_data_per_ticker_batch(stock_list=stock_list)
